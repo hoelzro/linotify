75debb83df87642d70df32bb1491518892236de3
Rob Hoelz <rob@hoelz.ro>
2014-04-02 08:00:21 -0500

    Remove backwards compatibility proxy table

--- linotify.c
+++ linotify.c
@@ -28,16 +28,6 @@
 #include <string.h>
 #include <unistd.h>
 
-#define INOTIFY_LIB_NAME "inotify"
-/* This definition causes a proxy
- * table to be placed into the global
- * namespace so that you may still use
- * _G.inotify.  If you wish to remove
- * this but you don't want to wait for
- * 0.3, simply remove this #define or
- * set it to 0. */
-#define LINOTIFY_01_COMPAT 1
-
 #define MT_NAME "INOTIFY_HANDLE"
 #define READ_BUFFER_SIZE 1024
 
@@ -264,32 +254,6 @@ static luaL_Reg handle_funcs[] = {
     lua_pushinteger(L, s);\
     lua_setfield(L, -2, #s);
 
-#if LINOTIFY_01_COMPAT && LUA_VERSION_NUM <= 501
-static int
-inotify_proxy_table__index(lua_State *L)
-{
-    if(! lua_toboolean(L, lua_upvalueindex(3))) {
-        lua_getfield(L, lua_upvalueindex(1), "print");
-        lua_pushliteral(L, "Relying on the global table installed by require 'inotify' is deprecated.  Please use the return value of require in the future.");
-        lua_call(L, 1, 0);
-
-        lua_getfield(L, lua_upvalueindex(1), INOTIFY_LIB_NAME);
-        if(lua_equal(L, 1, -1)) {
-            lua_pushvalue(L, lua_upvalueindex(2));
-            lua_setfield(L, lua_upvalueindex(1), INOTIFY_LIB_NAME);
-        }
-        lua_pop(L, 1);
-
-        lua_pushboolean(L, 1);
-        lua_replace(L, lua_upvalueindex(3));
-    }
-
-    lua_pushvalue(L, 2);
-    lua_gettable(L, lua_upvalueindex(2));
-    return 1;
-}
-#endif
-
 int luaopen_inotify(lua_State *L)
 {
     luaL_newmetatable(L, MT_NAME);
@@ -311,20 +275,6 @@ int luaopen_inotify(lua_State *L)
     luaL_setfuncs(L, inotify_funcs,0);
 #else
     luaL_register(L, NULL, inotify_funcs);
-#endif
-
-#if LINOTIFY_01_COMPAT && LUA_VERSION_NUM <= 501
-    lua_newtable(L);
-    lua_pushvalue(L, -1);
-    lua_setglobal(L, "inotify");
-    lua_newtable(L);
-    lua_pushvalue(L, LUA_GLOBALSINDEX);
-    lua_pushvalue(L, -4);
-    lua_pushboolean(L, 0);
-    lua_pushcclosure(L, inotify_proxy_table__index, 3);
-    lua_setfield(L, -2, "__index");
-    lua_setmetatable(L, -2);
-    lua_pop(L, 1);
 #endif
 
     register_constant(IN_ACCESS);
