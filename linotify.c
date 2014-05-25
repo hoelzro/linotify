2579ef017868d17e8e22896276099149addc6caa
Rob Hoelz <rob@hoelz.ro>
2014-05-25 04:40:32 -0500

    Update copyright in linotify.c

--- linotify.c
+++ linotify.c
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2009-2013 Robert Hoelz <rob@hoelz.ro>
+* Copyright (c) 2009-2014 Robert Hoelz <rob@hoelz.ro>
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
